23884711a2c6e75c1a1a37c5a46cea6e99f875e2
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-03-05 17:41:31 +0530

    Create digit.cpp

---
+++ digit.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define DIGITS_TO_REMOVE 3 // Assumed to be positive
+#define INPUT_SIZE 5 // Assumed to be greater than DIGITS_TO_REMOVE
+
+void recurse(int* input, int* output, int begin, int end){
+    int i;
+    int min = begin;
+
+    for (i = begin; i < end; ++i){
+        if (input[min] > input[i]){
+            min = i;
+        }
+    }
+    output[end - DIGITS_TO_REMOVE - 1] = input[min];
+
+    if (end < INPUT_SIZE){
+        recurse(input, output, min + 1, end + 1);
+    }
+}
+
+int main(void) {
+    int foo[] = { 2, 4, 6, 3, 5 };
+    int bar[INPUT_SIZE - DIGITS_TO_REMOVE];
+    int i;
+
+    recurse(foo, bar, 0, DIGITS_TO_REMOVE + 1);
+
+    for (int i = 0; i < INPUT_SIZE - DIGITS_TO_REMOVE; ++i){
+        printf("%d", bar[i]);
+    }
+    return 0;
+}
