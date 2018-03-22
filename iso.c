550ac0ebfada41fbee889319df820f7899b9f75d
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-03-22 17:49:25 +0530

    Update iso.c

--- iso.c
+++ iso.c
@@ -1,18 +1,33 @@
-#include<stdio.h>
-#include<string.h>
-struct student
+#include<bits/stdc++.h>
+#define NO_OF_CHARS 256
+using namespace std;
+bool isIsomorphic(str s1, str s2)
 {
-int id;
-char name[20];
-float percentage;
+ 
+    int m = s1.length(), n = s2.length();
+ if (m != n)
+      return false;
+   bool is_visited[NO_OF_CHARS] = {false};
+    int map[NO_OF_CHARS];
+    memset(map, -1, sizeof(map));
+    for (int i = 0; i < n; i++)
+    {
+        if (map[s1[i]] == -1)
+        {
+         if (is_visited[s2[i]] == true)
+                return false;
+           is_visited[s2[i]] = true;
+            map[s1[i]] = s2[i];
+ }
+            else if (map[s1[i]] != s2[i])
+            return false;
+    }
+ return true;
 }
-int main();
+ int main()
 {
-struct student record=0;
-record.id=1;
-strcpy percentage=86.5
-printf("is%d\n"record.id)
-printf("name is%s\n",record.name)
-printf("percentge is%f\n",record percentage;
-return 0;
+    str s1 = "bbfg";
+    str s2 = "nnop";
+   cout << isIsomorphic(s1, s2) << endl;
+   return 0;
 }
