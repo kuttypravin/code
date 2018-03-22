2f4d3a3e7165930b6ca60bf78fa33fbbefcd91b4
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-03-22 17:42:31 +0530

    Create roman.cpp

---
+++ roman.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int romanToInt(string s) 
+        int r = 0;
+        map<char,int>roman;
+        roman['I'] = 1;
+        roman['V'] = 5;
+        roman['X'] = 10;
+        roman['L'] = 50;
+        roman['C'] = 100;
+        roman['D'] = 500;
+        roman['M'] = 1000;
+        for (int i = 0; i < s.length() - 1; i ++) 
+        {
+            int curr =roman[s[i]];
+            int next = roman[s[i + 1]];
+            if (curr >= next) {
+                r += curr;
+            } else {
+                r -= curr;
+            }
+        }
+        return r + roman[s[s.length() - 1]];
+		return 0;
+}
