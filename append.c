b28d6b4368f575e1c6cbad6c00a3560b0400cf49
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-03-22 17:37:00 +0530

    Create append.c

---
+++ append.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+char str[30]
+printf("enter a string");
+gets(str);
+str lwr(str);
+puts("lower case string is;");
+printf("lower case string is");
+printf(str);
+}
