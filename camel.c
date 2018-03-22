21aa61097808a26857d6d2c99bf8b95a5d97fc13
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-03-22 17:55:35 +0530

    Create camel.c

---
+++ camel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int x=ccd;
+int y=mmn;
+printf("enter tha value a=ccd");
+scanf("%s",& x);
+printf("enter the value b=mmn");
+scanf("%d",& y);
+}
+else
+{
+print f("%d is changed the value")
+getch();
+}
