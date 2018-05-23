b32ca4e9bacc02cdafe23fa866fa38e59991f97f
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-05-23 11:31:59 +0530

    Update multiple.cpp

--- multiple.cpp
+++ multiple.cpp
@@ -4,11 +4,16 @@ int main()
 {
    int n,i,sum=0;
    cin>>n;
+    if((n >0)||(n <0))
+    {
    for(i=1;i<=5;i++)
    {
        sum=i*n;
       cout<<sum<<" ";
-   }
-   return 0;
+   }}
+    else 
+    {
+     cout<<"invalid input";   
+    }
+    return 0;
 }
-
