0d6ca1f0a929377babbea497c722725c2983c92f
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-05-24 14:54:31 +0530

    Create fibo.cpp

---
+++ fibo.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n,a,b,c,i;
+cin>>n;
+a=0;
+b=1;
+cout<<a<<" "<<b<<" ";
+for(i=1;i<=n;i++)
+{
+c=a+b;
+a=b;
+b=c;
+cout<<c<<" ";
+}
+return 0;
+}
