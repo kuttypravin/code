1806aa45e53d8db38b86924438ad22f73efb9799
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-05-24 09:36:50 +0530

    Create numeric.cpp

---
+++ numeric.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int s;
+    cin>>s;
+    if(s>0||s<0)
+        {
+            cout<<"yes";
+        }
+            else
+            {
+            cout<<"no";
+      }
+    return 0;
+    }
