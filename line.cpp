3457ddc3f545369d8cb34cad506da4086819c125
kuttypravin <37016587+kuttypravin@users.noreply.github.com>
2018-05-24 12:30:23 +0530

    Create line.cpp

---
+++ line.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin,str);
+    int count=0;
+    for (int i=0;i<str.length();i++)
+        {
+         if (str.at(i)=='.')
+          {
+           count++;
+          }
+        }
+        cout<<count;
+    return 0;
+}
